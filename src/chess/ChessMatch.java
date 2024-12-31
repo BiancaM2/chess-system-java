3ff134dd58c6758b25f859cbd5089488f6ab1252
BiancaM2 <bianca.morschbacher@gmail.com>
2024-12-31 17:28:48 -0300

    Implementando movimentos possiveis do rei

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -5,6 +5,7 @@ import boardgame.Piece;
 import boardgame.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
+import javafx.geometry.Pos;
 
 public class ChessMatch {
 
@@ -23,6 +24,12 @@ public class ChessMatch {
             }
         }
         return mat;
+    }
+
+    public boolean[][] possibleMoves(ChessPosition sourcePosition){
+        Position position = sourcePosition.toPosition();
+        validateSourcePosition(position);
+        return board.piece(position).possibleMoves();
     }
 
     public ChessPiece performChessMove (ChessPosition sourcePosition, ChessPosition targetPosition){
