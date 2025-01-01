895be7d2e22d31c3e129039bc4e30adf2cc69998
BiancaM2 <bianca.morschbacher@gmail.com>
2025-01-01 12:34:49 -0300

    Mostrando peças capturadas

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -5,13 +5,19 @@ import boardgame.Piece;
 import boardgame.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
-import javafx.geometry.Pos;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class ChessMatch {
 
     private int turn;
     private Color currentPlayer;
     private Board board;
+
+    private List<Piece> piecesOnTheBoard = new ArrayList<>();
+    private List<Piece> capturedPieces = new ArrayList<>();
 
     public ChessMatch(){
         board = new Board(8, 8);
@@ -58,6 +64,11 @@ public class ChessMatch {
         Piece p = board.removePiece(source);
         Piece capturedPiece = board.removePiece(target);
         board.placePiece(p, target);
+
+        if (capturedPiece != null){
+            piecesOnTheBoard.remove(capturedPiece);
+            capturedPieces.add(capturedPiece);
+        }
         return capturedPiece;
     }
 
@@ -66,7 +77,7 @@ public class ChessMatch {
             throw new ChessException("There is no piece on source position.");
         }
         if (currentPlayer != ((ChessPiece)board.piece(position)).getColor()){
-            throw new ChessException("A peça escolhida não é sua.");
+            throw new ChessException("The chosen piece is not yours.");
         }
         if (!board.piece(position).isThereAnyPossibleMove()){
             throw new ChessException("There is no possible moves for the chosen piece.");
@@ -86,6 +97,7 @@ public class ChessMatch {
 
     private void placeNewPiece(char column, int row, ChessPiece piece){
         board.placePiece(piece, new ChessPosition(column, row).toPosition());
+        piecesOnTheBoard.add(piece);
     }
 
     private void initialSetup() {
